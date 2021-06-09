9be453eeb5904c929a5df630ee095933e14cef23
Jack Butler <john.butler@uconn.edu>
2021-06-08 20:39:10 -0400

    Added shed + shed key

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,69 @@
 import React, { useState } from 'react';
 import './App.css';
-import { importRooms } from './Rooms';
+import { Action, importScenes } from './Scenes';
+import Menu from './Menu';
+import ActionButton from './ActionButton';
 
-const roomsById = importRooms();
-const STARTING_ROOM_ID = 'outside_front_door';
+const scenesById = importScenes();
+const STARTING_SCENE_ID = 'outside_front_door';
 
 const App = () => {
-  const [roomId, setRoomId] = useState(STARTING_ROOM_ID);
-  const room = roomsById[roomId];
+  const [sceneId, setSceneId] = useState(STARTING_SCENE_ID);
+  const room = scenesById[sceneId];
+  const [inventory, setInventory] = useState([] as string[]);
+
+  const isActionVisible = (action: Action) => {
+    if (action.requires) {
+      if (action.requires.items) {
+        if (!action.requires.items.every(item => inventory.includes(item))) {
+          return false;
+        }
+      }
+    }
+    if (action.forbids) {
+      if (action.forbids.items) {
+        if (action.forbids.items.find(item => inventory.includes(item))) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  const actionClick = (action: Action) => {
+    if (action.scene) {
+      setSceneId(action.scene);
+    }
+    if (action.adds) {
+      if (action.adds.items) {
+        // TODO: add distinct().  Doesn't matter since remove() will remove the duplicates
+        const updatedInventory = [...inventory, ...action.adds.items]
+        setInventory(updatedInventory);
+      }
+    }
+    if (action.removes) {
+      if (action.removes.items) {
+        // TODO: add distinct()
+        const { items } = action.removes;
+        const updatedInventory = inventory.filter(item => items && !items.includes(item));
+        setInventory(updatedInventory);
+      }
+    }
+  };
 
   return (
-    <div className="app">
-      <h1>{room.name}</h1>
-      <div className="description">
-        {room.description}
-      </div>
-      {room.actions.map(action => (
-        <div
-          key={action.ref}
-          className="action"
-          onClick={() => {
-            if (action.ref) {
-              setRoomId(action.ref);
-            }
-          }}
-        >
-          {action.text}
-        </div>
-      ))}
-    </div>
+    <Menu
+      title={room.name}
+      description={room.description}
+    >
+      {
+        room.actions
+          .filter(action => isActionVisible(action))
+          .map(action => (
+            <ActionButton text={action.text} onClick={() => actionClick(action)} />
+          ))
+      }
+    </Menu>
   );
 }
 
---
+++ src/Scenes.tsx
@@ -0,0 +1,121 @@
+import json from './scenes.json';
+
+/*
+ * This is extremely tedious; we'll delete it once we integrate with json-schema
+ */
+
+interface Scene {
+  id: string,
+  name: string,
+  description?: string,
+  actions: Action[]
+}
+
+type Action = {
+  scene?: string,
+  text: string,
+  requires?: {
+    items?: string[]
+  },
+  forbids?: {
+    items?: string[]
+  },
+  adds?: {
+    items?: string[]
+  },
+  removes?: {
+    items?: string[]
+  }
+}
+
+const importScenes: () => { [name: string]: Scene } = () => {
+  const scenesById: { [name: string]: Scene } = {};
+  json.forEach(obj => {
+    const room = _validateScene(obj);
+    const { id } = room;
+    scenesById[id] = room;
+  });
+  _validateRefIntegrity(scenesById);
+  return scenesById;
+}
+
+const _validateScene = (scene: any): Scene => {
+  _checkState(_isObject(scene), scene);
+  _checkState(scene.hasOwnProperty('id') && typeof scene.id === 'string', scene);
+  _checkState(scene.hasOwnProperty('name') && typeof scene.name === 'string', scene);
+  if (scene.hasOwnProperty('description')) {
+    _checkState(typeof scene.description === 'string', scene);
+  }
+  _checkState(scene.hasOwnProperty('actions') && Array.isArray(scene.actions), scene);
+  const actions = scene.actions.map((action: any) => _validateAction(action));
+
+  return {
+    ...scene,
+    actions
+  };
+};
+
+const _validateAction = (action: any): Action => {
+  _checkState(action.hasOwnProperty('text') && typeof action.text === 'string', action);
+  if (action.hasOwnProperty('scene')) {
+    _checkState(typeof action.scene === 'string', action);
+  }
+  if (action.hasOwnProperty('requires')) {
+    _checkState(_isObject(action.requires), action);
+    if (action.requires.hasOwnProperty('items')) {
+      _checkState(Array.isArray(action.requires.items), action);
+      action.requires.items.forEach((i: any) => _checkState(typeof i === 'string', action));
+    }
+  }
+  if (action.hasOwnProperty('forbids')) {
+    _checkState(_isObject(action.forbids), action);
+    if (action.forbids.hasOwnProperty('items')) {
+      _checkState(Array.isArray(action.forbids.items), action);
+      action.forbids.items.forEach((i: any) => _checkState(typeof i === 'string', action));
+    }
+  }
+  if (action.hasOwnProperty('adds')) {
+    _checkState(_isObject(action.adds), action);
+    if (action.adds.hasOwnProperty('items')) {
+      _checkState(Array.isArray(action.adds.items), action);
+      action.adds.items.forEach((i: any) => _checkState(typeof i === 'string', action));
+    }
+  }
+  if (action.hasOwnProperty('removes')) {
+    _checkState(_isObject(action.removes), action);
+    if (action.removes.hasOwnProperty('items')) {
+      _checkState(Array.isArray(action.removes.items), action);
+      action.removes.items.forEach((i: any) => _checkState(typeof i === 'string', action));
+    }
+  }
+  return action as Action;
+};
+
+/**
+ * Validate that all references to scene ids are valid
+ */
+const _validateRefIntegrity = (scenes: { [id: string]: Scene }) => {
+  Object.values(scenes).forEach(room => {
+    room.actions.forEach(action => {
+      if (action.scene) {
+        _checkState(scenes[action.scene] != null, action);
+      }
+    });
+  });
+}
+
+/**
+ * @param obj The contents of the object being checked, for logging purposes
+ */
+const _checkState = (condition: boolean, obj: any): void => {
+  if (!condition) {
+    throw new Error("Invalid definition: " + JSON.stringify(obj));
+  }
+}
+
+const _isObject = (o: object) => {
+  return typeof o === 'object' && !Array.isArray(o);
+}
+
+export { importScenes };
+export type { Scene, Action };
